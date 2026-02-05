1d574cb945b5cb383fe6af46cfa799f10edda703
David du Colombier <0intro@gmail.com>
2026-02-05 22:08:42 +0100

    run linters in GitHub Actions

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+	js.configs.recommended,
+	{
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				L: 'readonly',
+				html2canvas: 'readonly',
+				jsPDF: 'readonly'
+			}
+		},
+		rules: {
+			'indent': ['error', 'tab'],
+			'linebreak-style': ['error', 'unix'],
+			'quotes': ['error', 'single', { avoidEscape: true }],
+			'semi': ['error', 'always'],
+			'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+			'no-console': 'off'
+		}
+	}
+];
